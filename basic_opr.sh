478f2ee12a158adb275eabb6307c9b17006c1ffa
banslriya <riyabansal261198@gmail.com>
2019-07-27 14:50:40 +0530

    fifth

---
+++ basic_opr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "enter first no : "
+read x
+echo "enter second no : "
+read y
+echo "sum : "
+echo $(($x+$y))
+echo "sub : "
+echo $(($x-$y))
+echo "multi : "
+echo $(($x*$y))
+echo "div : "
+echo $(($x/$y))
